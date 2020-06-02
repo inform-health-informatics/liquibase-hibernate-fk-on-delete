2d45051b481973bd683fa482efd5687849f6c2fd
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-06-02 14:26:09 +0100

    Looks like only snapshot change is required

--- src/main/java/liquibase/ext/hibernate/diff/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Diff enabling on delete and on update functionality for foreign keys.
- */
-package liquibase.ext.hibernate.diff;
--- src/test/java/liquibase/ext/hibernate/database/ForeignKeyRuleTest.java
+++ src/test/java/liquibase/ext/hibernate/database/ForeignKeyRuleTest.java
@@ -16,8 +16,7 @@ import org.junit.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class ForeignKeyRuleTest {
     DatabaseSnapshot snapshot;
